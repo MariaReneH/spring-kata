aa69799df3a28d19a5dcdbd6d4e1a7cce417591d
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-07-23 23:23:17 -0400

    added test for student service implement

--- src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
+++ src/test/java/com/ss/web/app/service/StudentServiceImplTest.java
@@ -3,6 +3,10 @@ package com.ss.web.app.service;
 import com.ss.web.app.model.Student;
 import com.ss.web.app.repository.StudentRepo;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,9 +26,34 @@ class StudentServiceImplTest {
     assertEquals(student, studentExpected);
   }
 
-//  @Test
-//  public void findAllTest(){
-//    StudentService studentService = new StudentService();
-//    studentService
-//  }
+  @Test
+  public void testDeleteStudent() {
+    StudentRepo repo = mock(StudentRepo.class);
+    StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+
+    Student student = new Student(5L,"John", "Doe");
+
+    when(repo.save(student)).thenReturn(student);
+    when(repo.delete(student)).thenReturn(true);
+    boolean isDeleted = studentServiceImpl.delete(student);
+
+    assertEquals(true, isDeleted);
+  }
+  @Test
+  public void testFindStudent(){
+    StudentRepo repo = mock(StudentRepo.class);
+    StudentServiceImpl studentServiceImpl = new StudentServiceImpl(repo);
+
+    List<Student> students = new ArrayList<>();
+    students.add(new Student(1L,"John", "Doe"));
+    students.add(new Student(2L,"Jane", "Smith"));
+
+    // Act
+    when(repo.findAll()).thenReturn(students);
+    List<Student> result = studentServiceImpl.findAll();
+
+    // Assert
+    assertEquals(students, result);
+  }
+
 }
